ebcb9316486f8beaed9e0b06000185865fb3d640
hyt-88 <emma_hyt@yahoo.co.jp>
2025-06-09 02:49:08 +0900

    finish leaflet-test
    地図の表示できました。

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { MapContainer, TileLayer } from "react-leaflet";
+import { LatLng } from "leaflet";
+import 'leaflet/dist/leaflet.css';
 
 function Root() {
+    const position = new LatLng(51.505, -0.09);
     return (
         <>
-        // TODO: ここに地図だけを表示することを目指す。
+        <MapContainer center={position} zoom={13} style={{ height: '100vh', width: '100vw' }}>
+            <TileLayer 
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
         </>
     )
 }
