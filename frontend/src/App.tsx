6f3560725638402a58f12145dd20f5b9dea8c8d8
yuo_ota <yuuta.tsunashima@gmail.com>
2025-06-13 17:29:09 +0900

    Update ./front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Root from './Root'
+import NotFound from './NotFound'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    {/* TODO Rootコンポーネント > Postコンポーネント > RequireAuthコンポーネントの順で作成する。 */}
+                    {/* ホーム画面 */}
+                    <Route path="/" element={
+                        // <RequireAuth> // TODO 今後作る
+                            <Root />
+                        // </RequireAuth>
+                    } />
+                    {/* 投稿画面 */}
+                    <Route path="/post" element={
+                        <></> // TODO Postコンポーネントが作れたら消す
+                        // <RequireAuth> // TODO 今後作る
+                        //     <Post />
+                        // </RequireAuth>
+                    } />
+                    {/* NotFound画面 */}
+                    <Route path="*" element={
+                        <NotFound />
+                    } />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
