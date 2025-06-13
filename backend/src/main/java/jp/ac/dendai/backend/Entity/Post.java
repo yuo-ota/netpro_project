36d181efac461a6357d3dd0d339ce93e07d18a2a
yuo_ota <yuuta.tsunashima@gmail.com>
2025-06-13 16:59:36 +0900

    Revert "reset"
    This reverts commit 86d5342ecefe8122dc14dd2b3edffd5f02d80216.

---
+++ backend/src/main/java/jp/ac/dendai/backend/Entity/Post.java
@@ -0,0 +1,58 @@
+package jp.ac.dendai.backend.Entity;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private String postId;
+    private String pointId;
+    private String userId;
+    private LocalDateTime postedTime;
+    private String content;
+
+    public Post() {
+
+    }
+
+    public Post(String postId, String pointId, String userId, String content) {
+        this.postId = postId;
+        this.pointId = pointId;
+        this.userId = userId;
+        this.content = content;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getPointId() {
+        return pointId;
+    }
+
+    public void setPointId(String pointId) {
+        this.pointId = pointId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getPostedTime() {
+        return postedTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
