6a80f0e9de134f6db1c66387380def1dfbcbef6e
yuo_ota <yuuta.tsunashima@gmail.com>
2025-06-13 10:57:32 +0900

    Update Application.java
    SQL接続のためのメソッドを追加

--- backend/src/main/java/jp/ac/dendai/backend/util/NanoIdGenerator.java
+++ backend/src/main/java/jp/ac/dendai/backend/util/NanoIdGenerator.java
@@ -1,10 +1,13 @@
 package jp.ac.dendai.backend.util;
 
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
+
 public class NanoIdGenerator {
 
     public String generate() {
-        // TODO
         // ServiceファイルなどからNanoIdGenerator.generate()をすることでIDを発行することが目的
-        return null;
+        // もし独自の文字長などを作りたい場合はここに書く
+        // 参考: "https://github.com/aventrix/jnanoid"
+        return NanoIdUtils.randomNanoId();
     }
 }
