70f7f204bcd8d9a23725eafcd2cd3bdaef71bd10
hyt-88 <emma_hyt@yahoo.co.jp>
2025-06-20 15:14:32 +0900

    (PostList) Update

--- frontend/src/PostList.tsx
+++ frontend/src/PostList.tsx
@@ -51,76 +51,76 @@ function PostList() {
             </Tabs.List>
         </Tabs.Root>
 
-    <Box className='flex flex-col h-screen'>
-        <Box
-            className='
-                flex-1
-                overflow-y-auto
-                px-4
-                pt-4'
-        >
-            <VStack className='gap-4 flex items-stretch'>
-                {[...Array(20)].map((_, i) => (
-                    <Box
-                        key={i}
-                        className='
-                            p-4
-                            bg-white
-                            border-b
-                            border-gray-200
-                            last:border-b-0'
-                    >
-                        <Flex className='flex items-start'>
-                            <Box className='flex-1 pr-4'>
-                                <Text className='text-base mb-2'>
-                                    高瀬舟は京都の高瀬川を上下する小舟である。
-                                    徳川時代に京都の罪人が遠島を申し渡されると、本人の親類が牢屋敷へ呼び出されて、そこで暇乞いをすることを許された。
-                                    それから罪人は高瀬舟に載せられて、大阪へ回されることであった。
-                                </Text>
-                                <Text className='text-sm text-gray-500'>
-                                    2025/06/20 11:24
-                                </Text>
-                            </Box>
-                            <Flex
-                                className='
-                                    flex
-                                    flex-col
-                                    items-conter
-                                    mr-2'
-                            >
-                                <Box
-                                    className='
-                                        w-[12vh]
-                                        h-[12vh]
-                                        rounded-full
-                                        bg-white
-                                        border
-                                        border-gray-500
-                                        shadow-[0px_1px_3px_rgba(0, 0, 0, 0.1)]
-                                        flex
-                                        items-center
-                                        justify-center
-                                        mb-1'
-                                >
-                                    <FaRegThumbsUp className='text-[5vh] text-gray-500' />
+        <Box className='flex flex-col h-screen'>
+            <Box
+                className='
+                    flex-1
+                    overflow-y-auto
+                    px-4
+                    pt-4'
+            >
+                <VStack className='gap-4 flex items-stretch'>
+                    {[...Array(20)].map((_, i) => (
+                        <Box
+                            key={i}
+                            className='
+                                p-4
+                                bg-white
+                                border-b
+                                border-gray-200
+                                last:border-b-0'
+                        >
+                            <Flex className='flex items-start'>
+                                <Box className='flex-1 pr-4'>
+                                    <Text className='text-base mb-2'>
+                                        高瀬舟は京都の高瀬川を上下する小舟である。
+                                        徳川時代に京都の罪人が遠島を申し渡されると、本人の親類が牢屋敷へ呼び出されて、そこで暇乞いをすることを許された。
+                                        それから罪人は高瀬舟に載せられて、大阪へ回されることであった。
+                                    </Text>
+                                    <Text className='text-sm text-gray-500'>
+                                        2025/06/20 11:24
+                                    </Text>
                                 </Box>
-                                <Text className='text-sm font-bold'>
-                                    999
-                                </Text>
+                                <Flex
+                                    className='
+                                        flex
+                                        flex-col
+                                        items-center
+                                        mr-2'
+                                >
+                                    <Box
+                                        className='
+                                            w-[12vh]
+                                            h-[12vh]
+                                            rounded-full
+                                            bg-white
+                                            border
+                                            border-gray-500
+                                            shadow-[0px_1px_3px_rgba(0, 0, 0, 0.1)]
+                                            flex
+                                            items-center
+                                            justify-center
+                                            mb-1'
+                                    >
+                                        <FaRegThumbsUp className='text-[5vh] text-gray-500' />
+                                    </Box>
+                                    <Text className='text-sm font-bold'>
+                                        999
+                                    </Text>
+                                </Flex>
+                                
+                                <Box>
+                                    <FaEllipsisV className='text-lg text-gray-500' />
+                                </Box>
                             </Flex>
-                            
-                            <Box>
-                                <FaEllipsisV className='text-lg text-gray-500' />
-                            </Box>
-                        </Flex>
-                    </Box>
-                ))}
-                <Text className='text-sm text-gray-500'>
-                        コンテンツの終わりです。
-                </Text>
-            </VStack>
+                        </Box>
+                    ))}
+                    <Text className='text-sm text-gray-500'>
+                            コンテンツの終わりです。
+                    </Text>
+                </VStack>
+            </Box>
         </Box>
-    </Box>
     </>
     )
 }
