8e88ae1eec08e1b0456a287b3098811ac7a10fdd
yuo_ota <yuuta.tsunashima@gmail.com>
2025-06-26 01:04:01 +0900

    Update PointManageDto.java

--- backend/src/main/java/jp/ac/dendai/backend/Dto/PointManageDto.java
+++ backend/src/main/java/jp/ac/dendai/backend/Dto/PointManageDto.java
@@ -3,8 +3,6 @@ package jp.ac.dendai.backend.Dto;
 public class PointManageDto {
     PointDto symbolPoint;
     int pointCount;
-    // TODO なんでgoodCountを追加したのかわすれた要確認
-    int goodCount;
 
     public PointManageDto(PointDto symbolPoint) {
         this.symbolPoint = symbolPoint;
@@ -15,19 +13,11 @@ public class PointManageDto {
         pointCount++;
     }
 
-    public void addGoodCount(int i) {
-        goodCount += i;
-    }
-
     public PointDto getSymbolPoint() {
         return symbolPoint;
     }
 
     public int getPointCount() {
         return pointCount;
-    }
-
-    public int getGoodCount() {
-        return goodCount;
     }
 }
