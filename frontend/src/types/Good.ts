3132a49021cd7e57e74d421a173d12f6da779c31
yuo_ota <yuuta.tsunashima@gmail.com>
2025-06-25 11:50:46 +0900

    Update API Dto file

---
+++ frontend/src/types/Good.ts
@@ -0,0 +1,15 @@
+export type Good = {
+    postId: string;
+    isGooded: boolean;
+};
+
+export const isGood = (obj: unknown): obj is Good => {
+    if (typeof obj !== 'object' || obj === null) return false;
+
+    const good = obj as { [key: string]: unknown };
+
+    return (
+        typeof good.postId === 'string' &&
+        typeof good.isGooded === 'boolean'
+    );
+};
