7fd5189c964035ee463f868b5ee9eb9127c6fab5
yuo_ota <yuuta.tsunashima@gmail.com>
2025-07-06 17:41:48 +0900

    Delete BackendApplicationTests.java

--- backend/src/test/java/jp/ac/dendai/backend/BackendApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package jp.ac.dendai.backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendApplicationTests {
-
-}
