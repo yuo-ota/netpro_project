0f49943935755560e8709e82f1933d96ad3b41ec
yuo_ota <yuuta.tsunashima@gmail.com>
2025-07-07 22:48:40 +0900

    Update CorsConfig.java

--- backend/src/main/java/jp/ac/dendai/backend/Config/CorsConfig.java
+++ backend/src/main/java/jp/ac/dendai/backend/Config/CorsConfig.java
@@ -14,8 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedOrigins("https://coco-kite.net")
+                        .allowedOrigins("http://localhost:5173", "https://coco-kite.net")
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
                         .allowCredentials(true);
             }
